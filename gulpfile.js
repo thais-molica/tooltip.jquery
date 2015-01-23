8444c05565237caa1e49ffa61c71c2f1c0b6d187
thais-molica <thamoli@gmail.com>
2015-01-23 12:54:59 -0200

    Incluindo gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-var gulp = require( 'gulp' ) ;
-var sass = require( 'gulp-sass' ) ;
-var cssmin = require('cssmin');
+var gulp = require( 'gulp' ) ,
+	sass = require( 'gulp-sass' ) ;
 
-gulp.task( 'tcharam', function() {
+gulp.task( 'sass', function() {
   gulp.src( './jquery.tooltip/sass/style-tooltip.scss' )
         .pipe( sass() )
         .pipe( gulp.dest('./jquery.tooltip/css') ) ;
+} ) ;
+
+gulp.task( 'watch', function() {
+	gulp.watch( './jquery.tooltip/sass/*.scss', ['sass'] )
 } ) ;
