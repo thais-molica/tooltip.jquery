8e972602dc46095f6183e1992759c0e1a6676c48
thais-molica <thamoli@gmail.com>
2015-01-27 15:37:31 -0200

    Css Auto prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require( 'gulp' ) ,
-	sass = require( 'gulp-sass' ) ;
+	sass = require( 'gulp-sass' ) ,
+	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task( 'sass', function() {
   gulp.src( './jquery.tooltip/sass/style-tooltip.scss' )
+	  	.pipe(autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'] }))
         .pipe( sass() )
         .pipe( gulp.dest('./jquery.tooltip/css') ) ;
 } ) ;
